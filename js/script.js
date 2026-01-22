fb478f54acb2e7d8674ac447cee220b1d09c9358
Adil Shaikh <65012280+imask-dev@users.noreply.github.com>
2026-01-22 10:34:23 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -34,7 +34,7 @@ document.getElementById("enquiryForm").addEventListener("submit", function(e){
   const message = this.querySelector("textarea").value;
 
   // 1. Send to Google Sheet
-fetch("/api/submit", {
+fetch("https://aiahm.in/api/submit", {
   method: "POST",
   headers: { "Content-Type": "application/json" },
   body: JSON.stringify({ name, email, phone, course, message })
@@ -79,4 +79,5 @@ document.getElementById("closeSuccess").onclick = () => {
 };
 
 });
+
 
