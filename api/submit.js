1699a1e524b0626b53c3220f26cd510a9e1cb342
Adil Shaikh <65012280+imask-dev@users.noreply.github.com>
2026-01-22 09:15:15 +0530

    Update submit.js
    to work on real site

--- api/submit.js
+++ api/submit.js
@@ -1,22 +1,36 @@
 export default async function handler(req, res) {
+  const allowedOrigins = [
+    "https://aviindia-inky.vercel.app",
+    "https://www.aiahm.in",
+    "https://aiahm.in"
+  ];
+
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") return res.status(200).end();
   if (req.method !== "POST") return res.status(405).end();
 
   const data = req.body;
 
   if (!data.name || !data.phone) {
-    return res.status(400).json({ error: "Invalid" });
+    return res.status(400).json({ success: false });
   }
 
-  // Send to Google Sheet
   await fetch(process.env.SHEET_URL, {
     method: "POST",
     body: JSON.stringify(data),
   });
-
+  
   await fetch(process.env.MAILCHIMP_WEBHOOK, {
   method: "POST",
   body: JSON.stringify(data),
 });
-
+  
   return res.json({ success: true });
 }
