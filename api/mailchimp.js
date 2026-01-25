e43ee9a8278f6aa8472d4a6f052b745ae199cf86
Adil Shaikh <65012280+imask-dev@users.noreply.github.com>
2026-01-25 11:47:12 +0530

    Delete api directory

--- api/mailchimp.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method !== "POST") return res.status(200).end();
-
-  const data = req.body;
-
-  console.log("Mailchimp data:", data);
-
-  return res.status(200).json({ received: true });
-}
